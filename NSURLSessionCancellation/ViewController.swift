ba7a8851f4c9575040fc0c49866fe9fce342cb60
Shagun Madhikarmi <madhikarma@gmail.com>
2016-09-18 16:58:35 +0100

    Fixes and updates for demo / sample code - Fix for updating UI on background thread created by URL session - Refactored download handling logic for readability - Updated strings to use NSLocalizedString macro for future localisation

--- NSURLSessionCancellation/ViewController.swift
+++ NSURLSessionCancellation/ViewController.swift
@@ -10,70 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    private let session = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration())
+    // A sample URL that takes a while to download (so that cancellaton can be demonstrated)
+    
     private let url = NSURL(string: "http://yahoo.com")!
+    
+    /// A default URL session
+    
+    private let session = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration())
 
     
-    // MARK: - View lifecycle
-    
-    override func viewDidLoad() {
-        
-        super.viewDidLoad()
-    }
-    
-    
-    // MARK: - Actions
+    // MARK: - UI Actions
     
     @IBAction func downloadButtonPressed(sender: AnyObject) {
         
-    UIApplication.sharedApplication().networkActivityIndicatorVisible = true
+        self.setLoadingSpinnerVisible(true)
         
-        self.session.dataTaskWithURL(url) { (data, response, error) in
+        // Download data 
+        
+        self.session.dataTaskWithURL(self.url) { (data, response, error) in
             
-            UIApplication.sharedApplication().networkActivityIndicatorVisible = false
-            
-            var message: String = ""
-            var title: String = ""
-
-            if let downloadError = error {
+            dispatch_async(dispatch_get_main_queue(), { 
+             
+                self.setLoadingSpinnerVisible(false)
                 
-                if downloadError.domain == NSURLErrorDomain && downloadError.code == NSURLErrorCancelled {
-                  
-                    message = "Cancelled download for \(self.url) error: \(downloadError)"
-                    title = "Cancelled"
-                    
-                } else {
-                    
-                    message = "Error downloading \(self.url) error: \(downloadError)"
-                    title = "Error"
-                }
-     
-            } else if let _ = data {
+                let titleAndMessage = self.createAlertTitleAndMessage(error, data: data)
+                let title = titleAndMessage.0
+                let message = titleAndMessage.1
                 
-                message = "Success downloading data"
-                title = "Success"
-            }
+                self.showAlert(title, message: message)
+            })
             
-            let alertController = UIAlertController(title: title, message: message, preferredStyle: .Alert)
-            
-            let okAction = UIAlertAction(title: NSLocalizedString("Ok", comment: ""), style: UIAlertActionStyle.Cancel) { [weak self] (alertAction) -> Void in
-                
-                if let strongSelf = self {
-                    
-                    strongSelf.dismissViewControllerAnimated(true, completion: nil)
-                }
-            }
-            
-            alertController.addAction(okAction)
-            
-            self.presentViewController(alertController, animated: true, completion: nil)
-
-            
-            }.resume()
+        }.resume()
     }
-    
-    
-    // MARK: - UI Actions
     
     @IBAction func cancelAllButtonPressed(sender: AnyObject) {
         
@@ -84,5 +52,54 @@ class ViewController: UIViewController {
         
         self.session.cancelRequestForURL(self.url)
     }
+    
+    
+    // MARK: - Loading UI
+    
+     private func setLoadingSpinnerVisible(visible: Bool) {
+        
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = visible
+    }
+    
+    
+    // MARK: - Alerts
+    
+    private func showAlert(title: String, message: String) {
+        
+        // Create an alert controller with ok button 
+        
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+        alertController.addAction(UIAlertAction(title: NSLocalizedString("Ok", comment: ""), style: .Cancel, handler: nil))
+        
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+    private func createAlertTitleAndMessage(error: NSError?, data: NSData?) -> (String, String) {
+        
+        // Create title and message for an alert based on whether there was an error or if there is data
+        
+        var message: String = ""
+        var title: String = ""
+        
+        if let error = error {
+            
+            if error.domain == NSURLErrorDomain && error.code == NSURLErrorCancelled {
+                
+                title = NSLocalizedString("Cancelled", comment: "")
+                message = NSLocalizedString("Cancelled download for \(self.url) error: \(error)", comment: "")
+                
+            } else {
+                
+                title = NSLocalizedString("Error", comment: "")
+                message = NSLocalizedString("Error downloading \(self.url) error: \(error)", comment: "")
+            }
+            
+        } else if let _ = data {
+            
+            title = NSLocalizedString("Success", comment: "")
+            message = NSLocalizedString("Success downloading data", comment: "")
+        }
+        
+        return (title, message)
+    }
 }
-
